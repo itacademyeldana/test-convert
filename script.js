c7e637050b31e7bc3f41c57293afb1187af63d75
itacademyeldana <maksatbekovaeldana401@gmail.com>
2022-12-22 10:25:12 -0600

    add js,html

---
+++ script.js
@@ -0,0 +1,7 @@
+let op
+function func() {
+    let result
+    let kg_ = Number(document.getElementById("KG").value)
+    let us_ = Number(document.getElementById("US").value)
+    result = kg_ * us_
+    document.getElementById("result").innerHTML = result}
